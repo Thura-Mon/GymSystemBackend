fe3ad7dad55ecedbb6d982f412a967b7d4de6f43
Thura Mon <thuramon2004@gmail.com>
2025-06-28 23:49:03 +0630

    Update TotalMemberController.php

--- app/Http/Controllers/Owner/TotalMemberController.php
+++ app/Http/Controllers/Owner/TotalMemberController.php
@@ -21,7 +21,7 @@ class TotalMemberController extends Controller
     // Total members count with active status (m_flag = 0 or)
     public function totalActiveMembers(Request $request)
     {
-        $totalActiveMembers = \App\Models\member::where('status', 'active')->count();
+        $totalActiveMembers = \App\Models\member::where('m_flag', 0)->count();
         return response()->json([
             'total_active_members' => $totalActiveMembers
         ]);         
