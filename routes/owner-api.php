e61f0d558165b6df4694b3d6e4603a7889693fbf
Thura Mon <thuramon2004@gmail.com>
2025-06-30 01:40:27 +0630

    Update owner-api.php

--- routes/owner-api.php
+++ routes/owner-api.php
@@ -10,7 +10,7 @@ Route::post('/login',[AuthController::class, 'login'])->middleware(); // Admin A
 
 Route::post('/total-members', [\App\Http\Controllers\Owner\MemberController::class, 'totalMembers']); // Total members count
 
-Route::post('/total-active-members', [\App\Http\Controllers\Owner\MemberController::class, 'totalActiveMembers']); // Total active members count
+Route::get('/total-active-members', [\App\Http\Controllers\Owner\MemberController::class, 'totalActiveMembers']); // Total active members count
 
 Route::post('/total-inactive-members', [\App\Http\Controllers\Owner\MemberController::class, 'totalInactiveMembers']); // Total inactive members count
 
