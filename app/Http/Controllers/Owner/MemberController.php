e85e7d8064533891b9ec43338676555404ba4479
Thura Mon <thuramon2004@gmail.com>
2025-06-30 02:14:54 +0630

    Update MemberController.php

--- app/Http/Controllers/Owner/MemberController.php
+++ app/Http/Controllers/Owner/MemberController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
 
 class MemberController extends Controller
 {
     // Total members count
-    public function totalMembers(Request $request)
+    public function totalMembers(Response $response)
     {
         $totalMembers = \App\Models\member::count();
 
@@ -19,7 +20,7 @@ class MemberController extends Controller
 
 
     // Total members count with active status (m_flag = 0 or)
-    public function totalActiveMembers(Request $request)
+    public function totalActiveMembers(Response $response)
     {
         $totalActiveMembers = \App\Models\member::where('m_flag', 0)->count();
         return response()->json([
