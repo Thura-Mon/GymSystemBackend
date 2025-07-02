5147639a6149bb7f05f3136a0ed05d05eabd5247
Thura Mon <thuramon2004@gmail.com>
2025-07-03 00:28:56 +0630

    Update PurchaseController.php

--- app/Http/Controllers/Owner/PurchaseController.php
+++ app/Http/Controllers/Owner/PurchaseController.php
@@ -17,6 +17,6 @@ class PurchaseController extends Controller
         // Fetch purchases with that plan category (e.g., p_month)
         $purchases = \App\Models\Purchase::where('p_month', $planCategory)->get();
 
-        return response()->json($purchases);
+         return response()->json(['message' => 'success'], 200);
     }
 }
