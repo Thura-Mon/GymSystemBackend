f0853e1ad2d57a1eb35273583c259abdbdcee28f
Thura Mon <thuramon2004@gmail.com>
2025-07-03 00:37:02 +0630

    Update PurchaseController.php

--- app/Http/Controllers/Owner/PurchaseController.php
+++ app/Http/Controllers/Owner/PurchaseController.php
@@ -9,10 +9,11 @@ class PurchaseController extends Controller
     public function viewPurchase(Request $request)
     {
         $planCategory = $request->input('category');
+        $purchases = \App\Models\Purchase::where('p_month', $planCategory)->get();
 
         if (in_array($planCategory, [1, 2, 3])) {
-        
-            return response()->json(['message' => 'success'], 200);
+            return response()->json(['message' => 'success', $purchases],200);
+            
         }
 
         // Fetch purchases with that plan category (e.g., p_month)
