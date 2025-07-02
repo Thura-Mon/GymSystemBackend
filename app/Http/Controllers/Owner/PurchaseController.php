b57608e60f030bcda3091405832d0bdbfdc2ef0e
Thura Mon <thuramon2004@gmail.com>
2025-07-03 00:31:22 +0630

    Update PurchaseController.php

--- app/Http/Controllers/Owner/PurchaseController.php
+++ app/Http/Controllers/Owner/PurchaseController.php
@@ -10,13 +10,12 @@ class PurchaseController extends Controller
     {
         $planCategory = $request->input('category');
 
-        if (!in_array($planCategory, [1, 2, 3])) {
-            return response()->json(['error' => 'Invalid category selected'], 400);
+        if (in_array($planCategory, [1, 2, 3])) {
+        
+            return response()->json(['message' => 'success'], 200);
         }
 
         // Fetch purchases with that plan category (e.g., p_month)
-        $purchases = \App\Models\Purchase::where('p_month', $planCategory)->get();
-
-         return response()->json(['message' => 'success'], 200);
+      
     }
 }
