a9b3f4801864b4622ce8c27767073bfa42ba884f
Thura Mon <thuramon2004@gmail.com>
2025-07-03 00:49:24 +0630

    Update PurchaseController.php

--- app/Http/Controllers/Owner/PurchaseController.php
+++ app/Http/Controllers/Owner/PurchaseController.php
@@ -7,19 +7,12 @@ use Illuminate\Http\Request;
 class PurchaseController extends Controller
 {
     public function viewPurchase(Request $request)
-{
-    $planCategory = $request->input('category');
+    {
+        $planCategory = $request->input('category');
 
-    if (in_array($planCategory, [1, 2, 3])) {
-        $purchases = \App\Models\Purchase::where('p_month', $planCategory)->get();
-
-        return response()->json([
-            'message' => 'success',
-            'data' => $purchases
-        ], 200);
+        if (in_array($planCategory, [1, 2, 3])) {
+            return response()->json(['message' => 'Success'], 400);
+        }
+      
     }
-
-    return response()->json(['error' => 'Invalid category selected'], 400);
-}
-
 }
