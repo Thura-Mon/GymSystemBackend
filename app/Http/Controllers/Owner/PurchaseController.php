63aa9e03bae88607c7c9b9114d8bc54d2da4e3b1
Thura Mon <thuramon2004@gmail.com>
2025-07-03 22:44:10 +0630

    Update PurchaseController.php

--- app/Http/Controllers/Owner/PurchaseController.php
+++ app/Http/Controllers/Owner/PurchaseController.php
@@ -9,13 +9,11 @@ class PurchaseController extends Controller
     public function viewPurchase(Request $request)
     {
         $planCategory = $request->input('category');
-        $purchases = \App\Models\Purchase::where('p_month', $planCategory)->first();
+        $purchases = \App\Models\Purchase::where('p_id', $planCategory)->first();
 
         if ($planCategory == 1 || $planCategory == 2 || $planCategory == 3) {
             
-            return response()->json([
-                'purchases' => $purchases
-            ], 200);
+            return response()->json(['purchases' => $purchases], 200);
         }
       
     }
