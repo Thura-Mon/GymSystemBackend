d66a792c791e55ead4af7233c003cfab16cb1c3e
Thura Mon <thuramon2004@gmail.com>
2025-07-09 18:34:43 +0630

    Update BodyBuilder.php

--- app/Models/BodyBuilder.php
+++ app/Models/BodyBuilder.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class BodyBuilder extends Model
 {
-    //
+
+    protected $table = 'body_builders';
+
+    protected $primaryKey = 'b_id';
+
+    public $incrementing = true;
+
+    protected $keyType = 'int'; 
+
+     protected $fillable = [
+        'b_name',
+        'b_description',
+        'b_phone',
+        'b_dob',
+        'b_nrc',
+        'b_image',
+        'b_address',
+        'b_certificate'
+
+    ];
+
 }
