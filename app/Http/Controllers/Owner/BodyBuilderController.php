a3fa8483d109d7298a5068df5d536e62858fb4a9
Thura Mon <thuramon2004@gmail.com>
2025-07-11 10:59:30 +0630

    Update BodyBuilderController.php

--- app/Http/Controllers/Owner/BodyBuilderController.php
+++ app/Http/Controllers/Owner/BodyBuilderController.php
@@ -69,7 +69,7 @@ class BodyBuilderController extends Controller
     public function updatebodybuilder(Request $request)
     {
         $id = $request->input('b_id');
-        $trainer = \App\Models\BodyBuilder::where('b_id', $id)->get();
+        $trainer = \App\Models\BodyBuilder::where('b_id', $id)->first();
         if (!$trainer) {
             return response()->json([
                 'error' => 'trainer not found.'
