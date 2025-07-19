1b910a1c19f1cbd7145d631c5c0f5173651be190
Thura Mon <thuramon2004@gmail.com>
2025-07-19 14:37:47 +0630

    Update MemberController.php

--- app/Http/Controllers/Member/MemberController.php
+++ app/Http/Controllers/Member/MemberController.php
@@ -20,7 +20,7 @@ class MemberController extends Controller
         //check input date == today_date from member_days table
         $memberDays = MemberDay::where('today_date', $nowdate)->first();
 
-        if (!$memberDays) {
+        if ($memberDays->today_date == $nowdate) {
             return response()->json(['message' => 'You have benn attend for today'], 404);
         }
 
